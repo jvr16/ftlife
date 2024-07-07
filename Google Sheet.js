a002eb15a5189766be2742970f74aac42e8dd1a6
jvr16 <139189564+jvr16@users.noreply.github.com>
2024-07-07 23:37:03 +0800

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,6 +1,6 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwbJhjz4VzATzxr-7_dcbNdnOCwsYgQuHcO7H8Ayyibj22B8XxP_YcUav6amhtM2fWJaw/exec'
 
-const form = document.forms['contact-form']
+const form = document.forms['submission-form']
 
 form.addEventListener('submit', e => {
     e.preventDefault()
